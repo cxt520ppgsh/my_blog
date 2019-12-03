179900bff66e5f6140995a7194be44241e88ff1a
chenxutang <chenxutang@flyaudio.cn>
2019-12-03 11:17:53 +0800

    解决前端部署到服务器不能访问后端的问题

--- blog_user/front_end/config/prod.env.js
+++ blog_user/front_end/config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     // API_DOMAIN: '"http://luoxiaohui.cn"',
-    API_DOMAIN: '"http://localhost"', //cxt 生产环境用域名
+    API_DOMAIN: '"http://39.100.119.126"', //cxt 生产环境用域名
     API_PORT: '"4000"'
 }
--- blog_user/routers/router.go
+++ blog_user/routers/router.go
@@ -1,24 +1,25 @@
 package routers
 
 import (
-	"go_vue_blog/blog_user/controllers"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/plugins/cors"
+	"go_vue_blog/blog_user/controllers"
 )
 
 func init() {
 	// 这段代码放在router.go文件的init()的开头
-    beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowOrigins: 	  []string{"http://"+beego.AppConfig.String("front_end_domain")+":"+beego.AppConfig.String("front_end_port")},
-        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-        AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
-        ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
-        AllowCredentials: true,
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"http://39.100.119.126:8000",
+			"http://39.100.119.126:4000",
+			"http://" + beego.AppConfig.String("front_end_domain") + ":" + beego.AppConfig.String("front_end_port")},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
 	}))
 
-	beego.Router("/",&controllers.MainController{})
-	beego.Router("/articleDetail",&controllers.ArticleDetailController{})
-	beego.Router("/articleList",&controllers.ArticleListController{})
-	
-	
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/articleDetail", &controllers.ArticleDetailController{})
+	beego.Router("/articleList", &controllers.ArticleListController{})
+
 }
